5d43ca7b325e288ab982ac81934aca47273d0ef8
Woonsama <kws4510@naver.com>
2021-03-20 01:33:15 +0900

    (Client) Wave, WaveData, ScoreData 추가

--- Population/Assets/02.Scripts/Core/SaveData.cs
+++ Population/Assets/02.Scripts/Core/SaveData.cs
@@ -19,3 +19,39 @@ public class TestData
 
     }
 }
+
+public class WaveData
+{
+    public Wave wave;
+
+    WaveData()
+    {
+        wave.curWave = 1;
+        wave.isWaveAllClear = false;
+    }
+
+    public void WaveClear()
+    {
+        wave.WaveClear();
+    }
+
+    public bool GetIsWaveAllClear()
+    {
+        return wave.isWaveAllClear;
+    }
+}
+
+public class ScoreData
+{
+    public int score { get; private set; }
+
+    public ScoreData()
+    {
+        score = 0;
+    }
+
+    public void AddScore(int score)
+    {
+        this.score += score;
+    }
+}
