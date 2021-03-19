50a4e76cb3bf3d5e501d42c888a62a24ac108091
Woonsama <kws4510@naver.com>
2021-03-20 01:19:34 +0900

    (client) 플레이어 로직 수정

---
+++ Population/Assets/02.Scripts/Common/ExtensionMethod.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class ExtensionMethod
+{
+    public static void Log(this object value)
+    {
+#if Unity_Editor
+        Debug.Log(value);
+#endif
+    }
+}
