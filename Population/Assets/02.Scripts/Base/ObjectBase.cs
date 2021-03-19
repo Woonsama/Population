ce1d40a3fde1bd24f7492b707a2ec7dc621aac6c
Woonsama <kws4510@naver.com>
2021-03-20 00:58:30 +0900

    (Client) 캐릭터 이동로직 추가

---
+++ Population/Assets/02.Scripts/Base/ObjectBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBase : MonoBehaviour
+{
+    private void Awake()
+    {
+        OnAwake();
+    }
+
+    private void OnAwake()
+    {
+        StartCoroutine(OnAwakeCoroutine());
+    }
+
+    protected virtual IEnumerator OnAwakeCoroutine()
+    {
+        yield break;
+    }
+}
---
+++ Population/Assets/02.Scripts/Logic/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : ObjectBase, IMove
+{
+    [Header("스테이터스")]
+    public PlayerState state;
+
+    protected override IEnumerator OnAwakeCoroutine()
+    {
+
+        return base.OnAwakeCoroutine();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * 0.1f * state.moveSpeed;
+        float y = Input.GetAxisRaw("Vertical") * 0.1f * state.moveSpeed;
+        transform.Translate(new Vector2(x, y));
+    }
+}
+
+[System.Serializable]
+
+public class PlayerState
+{
+    [Header("이동 속도")]
+    public float moveSpeed;
+}
