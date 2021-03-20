b43a02d91aed1a85c6b6cb42b98825372bc954bc
Woonsama <kws4510@naver.com>
2021-03-20 14:31:42 +0900

    (Client) CameraController 추가

---
+++ Population/Assets/02.Scripts/Logic/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject target;
+
+    private void Update()
+    {
+        Follow();
+    }
+
+    public void SetTarget(GameObject obj)
+    {
+        target = obj;
+    }
+
+    public void DetachTarget()
+    {
+        target = null;
+    }
+
+    private void Follow()
+    {
+        if(target != null)
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        }
+    }
+}
