87e2575ecb4445c2cc7532d10504c4327ac9a344
Woonsama <kws4510@naver.com>
2021-03-21 07:00:24 +0900

    (Client) 타이틀 연결, 빌드 세팅 수정

---
+++ Population/Assets/02.Scripts/UI/Canvas_Title.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Canvas_Title : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Kws_Test");
+        }
+    }
+}
