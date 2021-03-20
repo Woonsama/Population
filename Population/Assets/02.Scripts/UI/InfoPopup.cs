07dd46cdef608944bde54c493da9f2d83d407df8
Songnari <na061300@naver.com>
2021-03-20 14:58:41 +0900

    (Client) 정보창 팝업기능 추가

---
+++ Population/Assets/02.Scripts/UI/InfoPopup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InfoPopup : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private GameObject popup = null;
+
+    private float applyHeight = 0f;
+
+    [SerializeField]
+    private RectTransform image = null;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        popup.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        popup.SetActive(false);
+    }
+
+}
