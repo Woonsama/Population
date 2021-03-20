6867e619f7d857ced3cbec53b6cf1b7de1a4eaad
Woonsama <kws4510@naver.com>
2021-03-21 05:39:33 +0900

    (Client) Add SoundManager

---
+++ Population/Assets/02.Scripts/Common/SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("오디오 - 배경음")]
+    public AudioSource audioSource_BGM;
+
+    public void ChangeBGM(AudioClip clip, bool loop)
+    {
+        audioSource_BGM.clip = clip;
+        audioSource_BGM.loop = loop;
+    }
+}
