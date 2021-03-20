b22bcc9f5e305f2a7a5334810f7dea1e139478b0
Woonsama <kws4510@naver.com>
2021-03-20 13:02:41 +0900

    (Client) 시민 로직 구현중

--- Population/Assets/02.Scripts/Common/Const.cs
+++ Population/Assets/02.Scripts/Common/Const.cs
@@ -8,4 +8,7 @@ public static class Const
     public const int c_Initial_Man_Cnt = 5;
     public const int c_Initial_WomenCnt = 5;
     public const int c_Initial_Old_Cnt = 5;
+
+    public const float c_Min_Citizen_StateChangeTime = 2.0f;
+    public const float c_Max_Citizen_StateChangeTime = 5.0f;
 }
--- Population/Assets/02.Scripts/Logic/Citizen.cs
+++ Population/Assets/02.Scripts/Logic/Citizen.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Citizen : ObjectBase
+public class Citizen : ObjectBase, IMove
 {
     public enum ECitizenType
     {
@@ -16,9 +16,86 @@ public class Citizen : ObjectBase
     [Header("시민 종류")]
     public ECitizenType eCitizenType;
 
+    [Header("스테이트")]
+    public CitizenState citizenState;
+
+    private float behiviourDelayTime = 0;
+
     protected override IEnumerator OnAwakeCoroutine()
     {
+        Behaviour_Coroutine();
         return base.OnAwakeCoroutine();
     }
 
+    private IEnumerator Behaviour_Coroutine()
+    {
+        ChangeState();
+        SetBehaviourChangeTime();
+        float time = 0;
+
+        while (!citizenState.isDie)
+        {
+            if(time >= behiviourDelayTime)
+            {
+                ChangeState();
+                SetBehaviourChangeTime();
+                time = 0;
+            }
+            else
+            {
+                Move();
+                time += Time.deltaTime;
+            }
+
+            yield return null;
+        }
+    }
+
+    public void Move()
+    {
+        switch (citizenState.eMoveState)
+        {
+            case CitizenState.EMoveState.LEFT:
+                transform.position += (Vector3)(Vector2.left * citizenState.moveSpeed * Time.deltaTime);
+                break;
+            case CitizenState.EMoveState.RIGHT:
+                transform.position += (Vector3)(Vector2.right * citizenState.moveSpeed * Time.deltaTime);
+                break;
+            case CitizenState.EMoveState.UP:
+                transform.position += (Vector3)(Vector2.up * citizenState.moveSpeed * Time.deltaTime);
+                break;
+            case CitizenState.EMoveState.DOWN:
+                transform.position += (Vector3)(Vector2.down * citizenState.moveSpeed * Time.deltaTime);
+                break;
+            case CitizenState.EMoveState.STOP:
+                transform.position += (Vector3)Vector2.zero;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ChangeState()
+    {
+        citizenState.eMoveState = (CitizenState.EMoveState)Random.Range((int)CitizenState.EMoveState.START, (int)CitizenState.EMoveState.END);
+    }
+
+    private void SetBehaviourChangeTime()
+    {
+        behiviourDelayTime = Random.Range(Const.c_Min_Citizen_StateChangeTime, Const.c_Max_Citizen_StateChangeTime);
+    }
+
+}
+
+[System.Serializable]
+
+public class CitizenState
+{
+    public enum EState {ALIVE, CATCHED, DIE }
+    public enum EMoveState {  LEFT, RIGHT, UP, DOWN, STOP , START = LEFT, END = STOP + 1, }
+
+    public EState eState = EState.ALIVE;
+    public EMoveState eMoveState = EMoveState.STOP;
+    public float moveSpeed;
+    public bool isDie;
 }
