745ac8d6bcc993607fbd6fb309ad9526d595d654
Woonsama <kws4510@naver.com>
2021-03-21 10:53:47 +0900

    (Client)

--- Population/Assets/02.Scripts/Base/SingletonMonoBase.cs
+++ Population/Assets/02.Scripts/Base/SingletonMonoBase.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class SingletonMonoBase<T> :MonoBehaviour where T :MonoBehaviour
